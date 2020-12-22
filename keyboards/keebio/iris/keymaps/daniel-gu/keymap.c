2987830e7f4cacb1ee08746e501ff3fbc622edaf
Daniel Gu <gu.daniel.y@gmail.com>
2020-12-22 02:20:06 -0500

    add layer functionality for rotary encoder

--- keyboards/keebio/iris/keymaps/daniel-gu/keymap.c
+++ keyboards/keebio/iris/keymaps/daniel-gu/keymap.c
@@ -114,24 +114,29 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
-        if (clockwise) {
-            tap_code(KC_RIGHT);
-        } else {
-            tap_code(KC_LEFT);
-        }
-    }
-    else if (index == 1) {
-        if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
-            tap_code(KC_VOLD);
-        }
-    }
-    else if (index == 2) {
-        if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
-            tap_code(KC_PGUP);
+        switch (biton32(layer_state))
+        {
+        case 2:
+          if (clockwise) {
+              tap_code(KC_UP);
+          } else {
+              tap_code(KC_DOWN);
+          }
+          break;
+        case 1:
+          if (clockwise) {
+              tap_code(KC_VOLU);
+          } else {
+              tap_code(KC_VOLD);
+          }
+          break;
+        default:
+          if (clockwise) {
+              tap_code(KC_RIGHT);
+          } else {
+              tap_code(KC_LEFT);
+          }
+          break;
         }
     }
 }
